92d9d5620ecb7a34c5e8bd712d5afb2ddcdfebfd
talipjan0999 <61458968+talipjan0999@users.noreply.github.com>
2020-05-12 17:06:38 +0200

    Update practice.java

--- src/tests/practice.java
+++ src/tests/practice.java
@@ -3,5 +3,12 @@ package tests;
 public class practice {
     public static void main(String[] args) {
 
+        
+        
+        
+        //this line is written from github
+        
+        
+        
     }
 }
